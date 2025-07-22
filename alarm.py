b9d4e10922a67628f5ed134d6e6cce8bbe3d6bc7
NamesAreHard Laptop <109428297+NamesAreHard6600@users.noreply.github.com>
2025-07-22 15:28:50 -0700

    Basic UI

--- alarm.py
+++ alarm.py
@@ -1,6 +1,27 @@
 # Alarm Class for CS361 Project
 # By: Joshua Knowles
+from tkinter import *
 
 class Alarm:
-    def __init__(self, title):
+    def __init__(self, ui, title, hour, minutes, pm):
+        self.ui = ui
         self.title = title
+        self.hour = hour
+        self.minutes = minutes
+        self.pm = pm
+
+        self.form_frame()
+
+    def form_frame(self):
+        self.frame = Frame(self.ui.root, bg="darkgrey", bd=3)
+        self.alarm_label = Label(self.frame, text=self.title, bg="darkgrey", font=("MS Reference Sans Serif",20))
+        self.alarm_label.grid(row=0, column=0, sticky="nw")
+
+        self.time_label = Label(self.frame,
+            text=f"{self.hour}:{self.minutes:02d} {'pm' if self.pm else 'am'}",
+            bg="darkgrey",
+            font=("MS Reference Sans Serif",20))
+
+        self.time_label.grid(row=1, column=0, sticky = "nw")
+
+        self.frame.grid(row=self.ui.get_num_alarms()+1, column=0, sticky="ew", padx=10, pady=5)
--- ui.py
+++ ui.py
@@ -9,23 +9,27 @@ class UI:
     def __init__(self):
         self.root = Tk()
         self.root.geometry("312x624")
-        self.root.title("Alarm.exe")
+        self.root.title("Wake.exe")
+        self.root.grid_columnconfigure(0, weight=1)
+        # self.root.grid_rowconfigure(0, weight=1)
 
-        self.title = Label(self.root, text="Alarm.exe")
-        self.title.grid(row=0, column=0)
+        self.title = Label(self.root, text="Wake.exe", font=("MS Sans Serif",36))
+        self.title.grid(row=0, column=0, sticky="new")
 
         self.main_frame = Frame(self.root, bg="darkgrey", bd=3)
-        self.main_frame.grid(row=1, column=0)
-
-        self.test = Label(self.main_frame, text="Test")
-        self.test.grid(row=0, column=0)
+        self.main_frame.grid(row=1, column=0, sticky="new")
 
         self.alarms = []
-        self.alarm_frames = []
+
+        self.add_alarm()
+        self.add_alarm()
 
     def add_alarm(self):
-        self.alarms.append(Alarm("Test"))
-        self.alarm_frames.append(Frame(self.root, ))
+        alarm = Alarm(self, "Test", 7, 30, False)
+        self.alarms.append(alarm)
+
+    def get_num_alarms(self):
+        return len(self.alarms)
 
     def display(self):
         self.root.mainloop()
